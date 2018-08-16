254e4eb790b3bb27fe545eb62450cf4034f5db10
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-16 18:08:47 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -19,6 +19,7 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
     @student.active = !@student.active
     @student.save
+    redirect_to(student_path(@student))
   end
   
   private
